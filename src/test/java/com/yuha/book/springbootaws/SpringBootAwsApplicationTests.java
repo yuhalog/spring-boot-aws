09c81902a9af1a14457b03bb810eb5acd9f9a861
YuHayeon <youha0930@gmail.com>
2022-04-13 01:43:30 +0900

    add: 프로젝트 생성

---
+++ src/test/java/com/yuha/book/springbootaws/SpringBootAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuha.book.springbootaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
