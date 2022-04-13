205bfa1364523fc5a0b338d797c6052263c0677d
YuHayeon <youha0930@gmail.com>
2022-04-14 00:17:18 +0900

    fix: Hello Controller 코드를 Lombok으로 전환

--- src/main/java/com/yuha/book/springbootaws/web/HelloController.java
+++ src/main/java/com/yuha/book/springbootaws/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.yuha.book.springbootaws.web;
 
+import com.yuha.book.springbootaws.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +11,12 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                           @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
     }
 
 }
