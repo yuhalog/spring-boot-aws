d2b9d6daed72cbfad2223ab381d0bda3455d7f29
YuHayeon <youha0930@gmail.com>
2022-04-15 01:59:29 +0900

    feat: 게시물 삭제 기능 추가

--- src/main/java/com/yuha/book/springbootaws/web/PostsApiController.java
+++ src/main/java/com/yuha/book/springbootaws/web/PostsApiController.java
@@ -26,4 +26,10 @@ public class PostsApiController {
     public PostsResponseDto findById (@PathVariable Long id) {
         return postService.findById(id);
     }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postService.delete(id);
+        return id;
+    }
 }
