e2feb712bdc650f069ad51dde71934b4e79c1e55
YuHayeon <youha0930@gmail.com>
2022-04-15 01:20:06 +0900

    feat: 게시물 수정 controller 기능 추가

--- src/main/java/com/yuha/book/springbootaws/web/IndexController.java
+++ src/main/java/com/yuha/book/springbootaws/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.yuha.book.springbootaws.web;
 
 import com.yuha.book.springbootaws.service.posts.PostsService;
+import com.yuha.book.springbootaws.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,6 +23,15 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String save() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String update(@PathVariable Long id, Model model) {
+
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 
 }
