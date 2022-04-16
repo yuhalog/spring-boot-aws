43f4970a18e1abf6968e01350588e501d3ffb1d6
YuHayeon <youha0930@gmail.com>
2022-04-16 20:52:26 +0900

    feat: User을 model에 저장하는 기능을 추가해서 구글 로그인 테스트

--- src/main/java/com/yuha/book/springbootaws/web/IndexController.java
+++ src/main/java/com/yuha/book/springbootaws/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.yuha.book.springbootaws.web;
 
+import com.yuha.book.springbootaws.domain.config.auth.dto.SessionUser;
 import com.yuha.book.springbootaws.service.posts.PostsService;
 import com.yuha.book.springbootaws.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findByDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
