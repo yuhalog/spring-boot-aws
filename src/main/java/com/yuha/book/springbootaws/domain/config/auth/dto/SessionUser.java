cbda40abee84068848781642cb0dcbb7887f9b10
YuHayeon <youha0930@gmail.com>
2022-04-16 21:30:20 +0900

    fix: Serializable 상속 받음

--- src/main/java/com/yuha/book/springbootaws/domain/config/auth/dto/SessionUser.java
+++ src/main/java/com/yuha/book/springbootaws/domain/config/auth/dto/SessionUser.java
@@ -3,8 +3,10 @@ package com.yuha.book.springbootaws.domain.config.auth.dto;
 import com.yuha.book.springbootaws.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
 
     private String name;
     private String email;
