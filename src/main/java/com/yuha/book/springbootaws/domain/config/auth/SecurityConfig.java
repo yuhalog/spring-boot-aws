b7dca15a822d635b589861d51afaf6e55543858f
YuHayeon <youha0930@gmail.com>
2022-04-16 20:51:24 +0900

    add: 스프링 시큐리티 설정 추가

---
+++ src/main/java/com/yuha/book/springbootaws/domain/config/auth/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.yuha.book.springbootaws.domain.config.auth;
+
+import com.yuha.book.springbootaws.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+
+                .and()
+                .authorizeRequests()
+                .antMatchers("/", "/css/**", "/images/**",
+                        "/js/**", "/h2-console/**").permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                .anyRequest().authenticated()
+
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
+    }
+}
---
+++ src/main/java/com/yuha/book/springbootaws/domain/config/auth/dto/SessionUser.java
@@ -0,0 +1,19 @@
+package com.yuha.book.springbootaws.domain.config.auth.dto;
+
+import com.yuha.book.springbootaws.domain.user.User;
+import lombok.Getter;
+
+@Getter
+public class SessionUser {
+
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+
+}
