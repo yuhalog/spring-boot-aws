9a725e74c8179bde8fed4a6035efef6fdbeea2d4
YuHayeon <youha0930@gmail.com>
2022-04-16 21:14:38 +0900

    add: 세션값 가져오는 부분 @LoginUser 어노테이션으로 대체

---
+++ src/main/java/com/yuha/book/springbootaws/domain/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.yuha.book.springbootaws.domain.config;
+
+import com.yuha.book.springbootaws.domain.config.auth.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(loginUserArgumentResolver);
+    }
+}
